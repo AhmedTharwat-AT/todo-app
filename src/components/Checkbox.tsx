daebf882867bbcb953a25177d09b045fdc69721c
iMorphex <ahmedsarwat3000@yahoo.com>
2024-01-16 05:45:33 +0200

    [Finished] deleted unused code/files

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -4,14 +4,11 @@ type Props = {
   className?: string;
 };
 
-// hover:text-white
-// hover:border-blue-400 hover:text-blue-200
-
 function Checkbox({ children, handler, className }: Props) {
   return (
     <button
       onClick={handler}
-      className={`${className} bp:h-7 bp:w-7 flex h-6 w-6 items-center justify-center rounded-full border  border-gray-300 text-3xl text-gray-300 transition-all dark:border-gray-700 dark:hover:border-blue-400`}
+      className={`${className} flex h-6 w-6 items-center justify-center rounded-full border border-gray-300 text-3xl  text-gray-300 transition-all dark:border-gray-700 dark:hover:border-blue-400 bp:h-7 bp:w-7`}
     >
       {children}
     </button>
